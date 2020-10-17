ff0b94c1400718285a37e05515777dbf28cffab5
apeshkov <apeshkov@evelopers.com>
2020-10-17 17:45:22 +0300

    [main] fix

--- src/lesson2/runner.ts
+++ src/lesson2/runner.ts
@@ -3,17 +3,17 @@ import { parser } from "./parser";
 import { firstPrioritiesCalc, secondPrioritiesCalc } from "./engine";
 
 export const runner = (line: string): number => {
-  const stack = parser(line);
+    const stack = parser(line);
 
-  if (stack === null) {
-    throw new TypeError("Unexpected string");
-  }
+    if (stack === null) {
+        throw new TypeError("Unexpected string");
+    }
 
-  const firstPrioritiesRes = firstPrioritiesCalc(stack);
+    const firstPrioritiesRes = firstPrioritiesCalc(stack);
 
-  if (firstPrioritiesRes.length === 1) {
-    return Number(firstPrioritiesRes[0]);
-  }
+    if (firstPrioritiesRes.length === 1) {
+        return Number(firstPrioritiesRes[0]);
+    }
 
-  return secondPrioritiesCalc(firstPrioritiesRes);
+    return secondPrioritiesCalc(firstPrioritiesRes);
 };
