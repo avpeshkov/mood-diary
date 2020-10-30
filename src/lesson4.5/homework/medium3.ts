8b2eda5648794161a5d6166ec9fb10af7ca79b7a
apeshkov <apeshkov@evelopers.com>
2020-10-30 21:24:37 +0300

    [main] lesson-4.5 base

---
+++ src/lesson4.5/homework/medium3.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FIXME = any;
+
+const orderStates = ["initial", "inWork", "buyingSupplies", "producing", "fullfilled"] as const;
+
+type OrderState = typeof orderStates[number];
+
+// Hint: type guards
+export const getUserOrderStates = (orderStates: OrderState[]): FIXME => orderStates.filter((state) => state !== "buyingSupplies" && state !== "producing");
