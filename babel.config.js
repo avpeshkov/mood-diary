c613600218f4f24a958f150027e28d1a44cda06b
apeshkov <apeshkov@evelopers.com>
2020-10-30 20:10:10 +0300

    [main] fix config for homework 2

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript", "@babel/preset-react"],
+    plugins: ["@babel/plugin-transform-arrow-functions", "@babel/plugin-proposal-class-properties"],
     env: {
         production: {
             plugins: ["emotion"],
@@ -9,3 +10,7 @@ module.exports = {
         },
     },
 };
+
+
+
+
